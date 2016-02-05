72ce1abfa38bf0797636801643324d3f4424d368
Hossam Saraya <hossam.saraya@gmail.com>
2016-02-05 20:06:49 +0200

    OOP User,Connection, UE

--- example/example1.js
+++ example/example1.js
@@ -3,7 +3,17 @@ var UE = require("../dist/UnificationEngine");
 var ue = new UE("b56063451547432d99111c91fd5d968b","695590bcf875546bf85c6358d3512ef8");
 ue.createUser().then(function(users){
     console.log(users);
+
+    ue.listUsers().then(function(users){
+        console.log(users);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
 }).catch(function(err){
     console.log("error:");
     console.log(err);
 })
+
+
+
