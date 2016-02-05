7bc01af0a26e29cf9ad9a7453a2d8e7d89f06c2e
Hossam Saraya <hossam.saraya@gmail.com>
2016-02-05 20:39:16 +0200

    delete user

--- example/example1.js
+++ example/example1.js
@@ -4,11 +4,21 @@ var ue = new UE("b56063451547432d99111c91fd5d968b","695590bcf875546bf85c6358d351
 ue.createUser().then(function(users){
     console.log(users);
 
-    ue.listUsers().then(function(users){
-        console.log(users);
+    ue.deleteUser(users)
+    .then(function(res){
+
+        console.log(res);
+        ue.listUsers().then(function(users){
+            console.log(users);
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+
     })
     .catch(function(err){
-        console.log(err);
+        console.log("Error deleting user");
+        console.log(err)
     })
 }).catch(function(err){
     console.log("error:");
