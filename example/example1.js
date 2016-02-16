2ede4d9feec11210a95e921827503646b4a6e006
Hossam Saraya <hossam.saraya@gmail.com>
2016-02-16 19:11:54 +0200

    send message formats

--- example/example1.js
+++ example/example1.js
@@ -4,56 +4,33 @@ var UE = require("../dist/UnificationEngine");
 var User = require("../dist/models/User")
 var ue = new UE("77e34324d1af445fab18907f092f898a","10f1f580972fd134f53189b81443dcb1");
 var async = require("async")
-var accessToken = "CAACEdEose0cBAIHk7xU861SDlIizx15qttn5jObH2ZCq5mYzaIJHRxMTs4oQHu6ZCeurEihQxQBtGymfNaUAbOC7j38Ty7Lc7qay45t2rga3xLD9km0wIAs168jsfMW7EYZBeZAzN0ZCtUlEdsiX0ENeY7tXALen28z9D5Oan1qRwgLAtwUTTWZB5gr6EvDtgtxNnTxj123cRiBPnJPuFc"
+var accessToken = "CAACEdEose0cBAFsVDWG53azkfubZCn6yrl0PxQRz131rbLwbWZBF4exmHVvlZBNs8QRhgupDEZB2ZBmQvu7nINk6kubZAooSf3lq6xkbtc3Dl1ZBA5kRNLUuciD9EcedUrqA7ue4QF8eM6b7Vn6ronpF012n4yXLOuuGGzlCFPfoijl7VPMU5ZAF8RYT5xWdIxNdJiS3DtHQZAgZDZD";
 // var hossamApp = new UE("6545514abd26476fb0fffaa76bfff90b","551a6c05c55741c9bc0e12a56b282435");
 
 
 var user = new User("200f6a0b-b45f-4742-b86e-f1fd2d5e118a","f6063346-0082-4bc1-8093-ebaa14955814")
-
 user.addConnection("facebook","facebook",accessToken)
 .then(function(connection){
     connection.sendMessage({
         "receivers":[
             {
-                "name":"Me",
-                "address":"test.test",
-                "Connector":"g"
+                "name":"me"
+            },
+            {
+                "name":"Page",
+                "id":"122"
             }
         ],
-        "subject":"test",
-        "parts": [
-            {
-                "id":"0",
-                "contentType":"binary",
-                "data":"msg2",
-                "type":"body"
-            },
-            {
-                "id":"1",
-                "contentType":"binary",
-                "type":"image_link",
-                "name":"filename",
-                "data":"http://www.hd-wallpapersdownload.com/upload/bulk-upload/desktop-pictures-of-cute-kittens-and-cats-wallpaper.jpg"
-            },
-            {
-                "id":"2",
-                "contentType":"binary",
-                "type":"link",
-                "data":"http://google.com"
-            },
-            {
-                "id":"3",
-                "contentType":"binary",
-                "type":"link_description",
-                "data":"link description here"
-            },
-            {
-                "id":"4",
-                "contentType":"binary",
-                "type":"link_title",
-                "data":"link title here"
+        "message":{
+            "subject":"test",
+            "body": "ABC",
+            "image":"http://politibits.blogs.tuscaloosanews.com/files/2010/07/sanford_big_dummy_navy_shirt.jpg",
+            "link":{
+                "uri": "http://google.com",
+                "description": "link desc",
+                "title":"link title"
             }
-        ]
+        }
     })
     .then(function(res){
         console.log(res)
