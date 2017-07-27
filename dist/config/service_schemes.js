e616068c343b01276bbcf3f73ad21ed6e3e19924
Jijo John <jijo@amt.in>
2017-07-27 08:56:24 +0530

    services

--- dist/config/service_schemes.js
+++ dist/config/service_schemes.js
@@ -3,4 +3,8 @@
  ******************************************/
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 exports["default"] = ["facebook", "twitter", "googleplus", "linkedin", "sms", "weibo", "whatsapp", "smtp", "telegram", "viberpa", "kik", "line", "sendgrid", "threema", "fbmessenger", "skype", "pinterest", "kakaostory", "fbworkplace", "viber", "tumblr", "wechat"];
+module.exports = exports["default"];
